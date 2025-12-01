c399eeb1afebb9ea2fbdf2e8d208359dab2739e8
anatk99-del <anatk99@gmail.com>
2025-12-01 12:29:27 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+
   return {
-    base: '/btl02/',   // ← חובה עבור GitHub Pages
+    base: '/btl02/',   // ← חובה עבור GitHub Pages !!!
 
     server: {
       port: 3000,
